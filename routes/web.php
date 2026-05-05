45e2f1e2c243a561784d14d8ec15bd89861f85a3
Gzbrr08 <martinsgabriel2148@gmail.com>
2026-05-05 17:52:46 -0300

    remove empty line in users route section

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Route;
     })->name('home');
 
     Route::get('/users', [App\Http\Controllers\UserController::class, 'index'])->name('users.index');
-
+..
     Route::get('users/create', [App\Http\Controllers\UserController::class, 'create'])->name('users.create');
     Route::post('users/create', [App\Http\Controllers\UserController::class, 'store'])->name('users.store');
 
